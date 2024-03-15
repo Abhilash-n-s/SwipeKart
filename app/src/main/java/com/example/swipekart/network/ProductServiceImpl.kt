a6bb9dac8048176bffcdf2ad9578162ae9066091
Abhilash-n-s <ns.abhilash9@gmail.com>
2024-03-15 19:58:55 +0530

    Added API Timeout

--- app/src/main/java/com/example/swipekart/network/ProductServiceImpl.kt
+++ app/src/main/java/com/example/swipekart/network/ProductServiceImpl.kt
@@ -4,17 +4,25 @@ import com.example.swipekart.Utils
 import com.example.swipekart.model.Product
 import com.example.swipekart.model.ProductResponse
 import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ProductServiceImpl:ProductService {
     private val productService: ProductService
 
     init {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
         val retrofit = Retrofit.Builder()
             .baseUrl(Utils().BASEURL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
